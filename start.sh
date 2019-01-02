ac12da857e52fb5d12f73cdf4c9fb9ccd4fdafe3
Thanh Vo <steve@vieted.com>
2019-01-02 13:51:08 +0700

    add prod config and build.js

--- start.sh
+++ start.sh
@@ -15,8 +15,10 @@ fi
 echo "Overwriting webpack configurations..."
 cp ./rocketize-webpack/config/webpack.config.dev.js ./node_modules/react-scripts/config/webpack.config.dev.js
 cp ./rocketize-webpack/config/webpackDevServer.config.js ./node_modules/react-scripts/config/webpackDevServer.config.js
+cp ./rocketize-webpack/config/webpack.config.prod.js ./node_modules/react-scripts/config/webpack.config.prod.js
 cp ./rocketize-webpack/config/paths.js ./node_modules/react-scripts/config/paths.js
 cp ./rocketize-webpack/scripts/start.js ./node_modules/react-scripts/scripts/start.js
+cp ./rocketize-webpack/scripts/build.js ./node_modules/react-scripts/scripts/build.js
 
 cd ./node_modules/react-scripts
 npm i happypack hard-source-webpack-plugin autodll-webpack-plugin@0.3
